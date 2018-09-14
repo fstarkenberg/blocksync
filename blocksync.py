1b7c2436fd17d95a7cb2157c25b4d6b5168592b4
Fredrik Starkenberg <starkenbergfredrik@gmail.com>
2018-09-14 21:22:02 +0000

    Nicer progress bar

--- blocksync.py
+++ blocksync.py
@@ -2,10 +2,12 @@
 
 import sys
 import os
+from datetime import datetime
 import hashlib
 
-BLOCK_DEVICE = 'disk'
-BLOCK_SIZE = 4096
+STARTTIME = datetime.now()
+BLOCK_DEVICE = '/dev/vda'
+BLOCK_SIZE = 16384
 
 def DEVICE_SIZE(BLOCK_DEVICE):
 	with open(BLOCK_DEVICE, 'rb') as f:
@@ -14,10 +16,15 @@ def DEVICE_SIZE(BLOCK_DEVICE):
 	return SIZE
 
 def progress(SIZE, CURRENT):
-    #return int(CURRENT/SIZE * 100)
-	SIZE_GB = int(SIZE/1000000000)
-	CURRENT_GB = int(CURRENT/1000000000)
-	print("%d/%d" % (CURRENT_GB, SIZE_GB), end='\r')
+    SIZE_GB = int(SIZE/1000000000)
+    CURRENT_GB = int(CURRENT/1000000000)
+    CURRENT_MB = int(CURRENT/1000000)
+    CURRENT_TIME = datetime.now() - STARTTIME
+    if CURRENT_TIME.seconds is 0:
+        SPEED = 0
+    else:
+        SPEED = CURRENT_MB / CURRENT_TIME.seconds
+    return "%d GB/%d GB - %d MB/s - %s" % (CURRENT_GB, SIZE_GB, SPEED, CURRENT_TIME)
 
 if len(sys.argv) < 2:
     print("USAGE: %s --generate | --compare /dev/blkdevice" % sys.argv[0])
@@ -28,23 +35,28 @@ SIZE = DEVICE_SIZE(BLOCK_DEVICE)
 if sys.argv[1] == '--generate':
     with open(BLOCK_DEVICE, 'rb') as f, open('disk.md5', 'w') as h:
         while f.tell() < SIZE:
-            progress(SIZE, f.tell())
+            p = progress(SIZE, f.tell())
+            print(p, end='\r')
             read_data = f.read(BLOCK_SIZE)
             m = hashlib.md5()
             m.update(read_data)
             h.write(m.hexdigest())
             h.write('\n')
+    print(p)
 
 elif sys.argv[1] == '--compare':
     with open(BLOCK_DEVICE, 'rb') as f, open('disk.md5', 'r') as h:
         CHANGE_COUNTER = 0
         for hash in h.readlines():
             read_data = f.read(BLOCK_SIZE)
+            p = progress(SIZE, f.tell())
+            print(p, end='\r')
             m = hashlib.md5()
             m.update(read_data)
             if m.hexdigest() == hash.strip():
                 pass
             else:
                 CHANGE_COUNTER += 1
-    print("%d blocks have changed" % CHANGE_COUNTER)
+    print(p)
+    print("%d blocks have changed. ~%d MB." % (CHANGE_COUNTER, (CHANGE_COUNTER * BLOCK_SIZE)/1000000))
 
